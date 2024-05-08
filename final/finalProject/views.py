776b1720587ebac84e6e62fa1ec73c052f08bcc4
Alexandra Salazar <138623509+Alex000127@users.noreply.github.com>
2024-05-08 15:24:56 -0600

    avances x

--- final/finalProject/views.py
+++ final/finalProject/views.py
@@ -9,26 +9,4 @@ def index(request):
     return render(request,"index.html")
 
 def login(request):
-    if request.method == 'POST':
-        user_type = request.POST.get('user_type', None)
-        username = request.POST.get('username', None)
-        password = request.POST.get('password', None)
-
-        if user_type == 'student':
-            # Authenticate against your custom User model
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                # Redirect to student dashboard or any desired page
-            else:
-                # Render error.html if authentication fails
-                return render(request, 'error.html', {'error_message': 'Invalid username or password.'})
-
-        elif user_type == 'staff':
-            # Similarly authenticate staff user
-            pass
-        else:
-            # Render error.html for invalid user type
-            return render(request, 'error.html', {'error_message': 'Invalid user type.'})
-
-    return render(request, 'login.html')
+    return render(request,"login.html")
